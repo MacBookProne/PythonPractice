5a23d27944cb9e1ddc0f65bb675f6a924b99edd1
Justin ODea <odeajustin@gmail.com>
2020-03-12 10:51:15 -0600

    A stack and Queue practice.

---
+++ stackandqueses.py
@@ -0,0 +1,19 @@
+
+print(["Nike","Under Armor", "lululemon", " Addidas", "Reebok"])
+print("")
+
+queue = ["Nike","Under Armor", "lululemon", " Addidas", "Reebok"]
+queue.remove(queue[-1])
+queue.append("Patagonia")
+
+
+print(" > > > > Queue > > > >")
+print(queue)
+print("")
+
+stack = ["Nike","Under Armor", "lululemon", " Addidas", "Reebok"]
+stack.pop()
+stack.append("Patagonia")
+
+print(" > > > > Stack > > > >")
+print(stack)
