9ed3bdd92495cd53fd37eacbdae326ae838af9af
Justin ODea <odeajustin@gmail.com>
2020-03-13 15:49:36 -0600

    commented

--- Fib_sequence.py
+++ Fib_sequence.py
@@ -1,15 +1,15 @@
-def fib_sequence(n):
+def fib_sequence(n): #defines the function
 
     if n<= 1:
         return n
     else:
-        return(fib_sequence(n-1)+ fib_sequence(n-2))
-nterms = 25
+        return(fib_sequence(n-1) + fib_sequence(n-2)) # each number is the sum of the two previous ones
+nterms = 25 #how many numbers we want to find.
 
 
-if nterms <= 0:
+if nterms <= 0:  #if numbers are less than 0
     print("Enter a postive integer.")
-else:
+else: #else print
     print("Fibonacci Sequence:")
     for i in range(nterms):
         print(fib_sequence(i))
