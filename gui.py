81009a951dbf7c5a412bb672c5fbaaa190269611
Justin ODea <odeajustin@gmail.com>
2020-03-21 14:47:21 -0600

    addedna bigger button

--- gui.py
+++ gui.py
@@ -11,11 +11,12 @@ top.geometry("500x500")
 
 
 def helloCallBack():
-    
+
    tkMessageBox.showinfo( "Hello Python", "Hello World")
 
 
-j = Tkinter.Button(top, text ="Hello", command = helloCallBack)
+j = Tkinter.Button(top, text ="Hello", command = helloCallBack, height = 50, width = 50)
+#j = Tkinter.Button(top, text = "Send", height = 100, width = 100)
 
 
 
